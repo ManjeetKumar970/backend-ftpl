05d104d86ef61916a7119795a6c4d535520f1fcf
Bholanath <bholanathbarik9748@gmail.com>
2025-02-24 21:42:18 +0530

    Feat: Implement rate limit according to ip

---
+++ src/common/middleware/rate-limit.middleware.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 10 minutes
+  max: 10, // Limit each IP to 100 requests per windowMs
+  message: {
+    success: false,
+    message: 'Too many requests from this IP, please try again later',
+  },
+  keyGenerator: (req: Request) => req.ip, // Extract IP address
+});
+
+@Injectable()
+export class RateLimitMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    limiter(req, res, next);
+  }
+}
