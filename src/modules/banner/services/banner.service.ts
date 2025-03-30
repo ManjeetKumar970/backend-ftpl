70dc60718c642160a8c444c08dac55193ae26017
Bholanath <bholanathbarik9748@gmail.com>
2025-03-30 21:26:56 +0530

    IMPV: Improve Banner controller and services get token from header

--- src/modules/banner/services/banner.service.ts
+++ src/modules/banner/services/banner.service.ts
@@ -5,34 +5,77 @@ import { CreateBannerDto } from '../dto/banner.dto';
 import { FileUploadService } from 'src/modules/file-upload/services/file-upload.service';
 import { getUserById } from 'src/utils/user.utils';
 import { Role } from 'src/enums/role.enum';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class BannerService {
   constructor(
     @InjectEntityManager() private readonly entityManager: EntityManager,
     private readonly fileUploadService: FileUploadService,
+    private jwtService: JwtService,
   ) {}
 
-  async createBanner(body: CreateBannerDto): Promise<{ message: string }> {
-    const userExists = await getUserById(this.entityManager, body.user_id);
+  async createBanner(
+    body: CreateBannerDto,
+    authHeader: string,
+  ): Promise<{ message: string }> {
+    try {
+      const token = authHeader.split(' ')[1];
+      const decoded: any = this.jwtService.decode(token);
 
-    if (!userExists || userExists?.user_role !== Role.ADMIN) {
-      throw new UnauthorizedException('Access denied: Admins only');
+      const userExists = await getUserById(this.entityManager, decoded.userId);
+
+      if (!userExists || userExists?.user_role !== Role.ADMIN) {
+        throw new UnauthorizedException('Access denied: Admins only');
+      }
+
+      await this.entityManager.query(
+        `INSERT INTO "banner" (user_id , name , file_id, head_description, sub_description, btn_link, is_active) VALUES ($1 , $2 , $3 , $4 , $5 , $6, $7)`,
+        [
+          decoded.userId,
+          body.name,
+          body.file_id,
+          body.head_description,
+          body.sub_description,
+          body.btn_link,
+          true,
+        ],
+      );
+      return { message: 'Banner Created successfully' };
+    } catch (error) {
+      throw error;
     }
+  }
 
-    await this.entityManager.query(
-      `INSERT INTO "banner" (user_id , name , file_id, head_description, sub_description, btn_link, is_active) VALUES ($1 , $2 , $3 , $4 , $5 , $6, $7)`,
-      [
-        body.user_id,
-        body.name,
-        body.file_id,
-        body.head_description,
-        body.sub_description,
-        body.btn_link,
-        true,
-      ],
-    );
-    return { message: 'Banner Created successfully' };
+  async updateBanner(
+    body: CreateBannerDto,
+    authHeader: string,
+  ): Promise<{ message: string }> {
+    try {
+      const token = authHeader.split(' ')[1];
+      const decoded: any = this.jwtService.decode(token);
+
+      const userExists = await getUserById(this.entityManager, decoded.userId);
+
+      if (!userExists || userExists?.user_role !== Role.ADMIN) {
+        throw new UnauthorizedException('Access denied: Admins only');
+      }
+
+      await this.entityManager.query(
+        `UPDATE "banner" SET name = $1, file_id = $2, head_description = $3, sub_description = $4, btn_link = $5, is_active = $6`,
+        [
+          body.name,
+          body.file_id,
+          body.head_description,
+          body.sub_description,
+          body.btn_link,
+          true,
+        ],
+      );
+      return { message: 'Banner Created successfully' };
+    } catch (error) {
+      throw error;
+    }
   }
 
   async getBannerAllBanner(
@@ -85,10 +128,14 @@ export class BannerService {
   }
 
   async deleteBannerByUserID(id: string): Promise<{ message: string }> {
-    await this.entityManager.query(
-      'UPDATE "banner" SET is_active = $2 WHERE id = $1',
-      [id, false],
-    );
-    return { message: 'Banner Deleted successfully' };
+    try {
+      await this.entityManager.query(
+        'UPDATE "banner" SET is_active = $2 WHERE id = $1',
+        [id, false],
+      );
+      return { message: 'Banner Deleted successfully' };
+    } catch (error) {
+      throw error;
+    }
   }
 }
